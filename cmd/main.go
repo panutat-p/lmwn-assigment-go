653dbcada6886b700f570c8bb097f2ef0572c73f
panutat-p <pp.panutat@gmail.com>
2023-04-30 23:55:36 +0700

    feat: change to Release mode

--- cmd/main.go
+++ cmd/main.go
@@ -15,12 +15,13 @@ func main() {
 	report := src.NewReport("https://static.wongnai.com/devinterview/covid-cases.json", 5*time.Second)
 	handler := src.NewHandler(report)
 
-	gin.SetMode(gin.DebugMode)
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 
 	router.GET("/", handler.Health)
 	router.GET("/report", handler.GetReport)
 	router.GET("/summary", handler.GetSummary)
+	log.Println("Web server is listening on port", Port)
 	err := router.Run(":" + Port)
 	if err != nil {
 		log.Println("Failed to run the web server, err:", err)
